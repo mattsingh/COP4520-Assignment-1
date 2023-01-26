616575a506137cfd6d6b04efdc9469031ee48dba
Matthew Singh <mattsingh@hotmail.com>
2023-01-26 18:52:30 -0500

    Remove Main.java & rename Eratosthenes.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -8,70 +8,109 @@ public class Main {
     public static int MIN = 2;
     public static int MAX = (int) Math.pow(10, 8);
     public static int NUM_THREADS = 8;
-    // private boolean primes[] = new boolean[MAX];
-    
+
     public static void main(String[] args) throws InterruptedException {
-        List<int[]> primes = new SynchronizedArrayList<int[]>();
-        primes.add(new int[] { 3, 3 });
-
+        boolean primes[] = new boolean[MAX + 1];
+        Arrays.fill(primes, true);
         ExecutorService threadExecutor = Executors.newFixedThreadPool(8);
+        Metrics metrics = new Metrics();
 
-        for (int i = 2; i < MAX; i++) {
-            threadExecutor.execute(new PrimeFinderThread(i, primes));
+        metrics.start();
+        for (int i = 2; i * i < MAX; i++) {
+            // If prime[p] is not changed, then it is a prime
+            if (primes[i])
+                threadExecutor.execute(new Eratosthenes(i, primes, metrics));
         }
 
         threadExecutor.shutdown();
         threadExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        metrics.end();
+        metrics.calculateNumAndSumPrimes(primes);
+        metrics.output();
     }
 }
 
-class PrimeFinderThread implements Runnable {
+class Eratosthenes implements Runnable {
     private int n;
-    // private boolean primes[];
-    private List<int[]> primes;
-    private int primeCounter;
+    private boolean primes[];
+    private Metrics metrics;
 
-    public PrimeFinderThread(int n, List<int[]> primes) {
+    public Eratosthenes(int n, boolean primes[], Metrics metrics) {
         this.n = n;
         this.primes = primes;
-        this.primeCounter = 0;
+        this.metrics = metrics;
     }
 
     @Override
     public void run() {
-        boolean isPrime = true;
-        for (int i = 0; i < primes.size(); i++) {
-            int prime[] = primes.get(i);
-            while(prime[1] < n)
-                prime[1] += prime[0];
-            if (prime[1] == n) {
-                isPrime = false;
-                break;
-            }
+        // record start time
+        long startTime = System.currentTimeMillis();
+        int iterations = 0;
+        // Update all multiples of p
+        for (int i = n * n; i <= Main.MAX; i += n) {
+            primes[i] = false;
+            iterations++;
         }
-        if (isPrime) {
-            primes.add(new int[] { n, n });
-            primeCounter++;
-            System.out.println(n + " is prime");
-        }
+        // record end time
+        long endTime = System.currentTimeMillis();
+        metrics.recordIterationsPerThread(Thread.currentThread().getName(), iterations);
+        metrics.recordDurationPerThread(Thread.currentThread().getName(), endTime - startTime);
     }
 }
 
-class SynchronizedArrayList<T> extends ArrayList<T> {
-    private static final long serialVersionUID = 1L;
+class Metrics {
+    public long startTime;
+    public long endTime;
+    public long numPrimes;
+    public long sumPrimes;
 
-    @Override
-    public synchronized boolean add(T e) {
-        return super.add(e);
+    // list of threads and their iterations
+    HashMap<String, Integer> threadIterations = new HashMap<String, Integer>();
+    HashMap<String, Long> threadDurations = new HashMap<String, Long>();
+
+    public Metrics() {
+        numPrimes = 0;
+        sumPrimes = 0;
     }
 
-    @Override
-    public synchronized T get(int index) {
-        return super.get(index);
+    public void start() {
+        startTime = System.currentTimeMillis();
     }
 
-    @Override
-    public synchronized int size() {
-        return super.size();
+    public void end() {
+        endTime = System.currentTimeMillis();
+    }
+
+    public void output() {
+        System.out.println("Time: " + (endTime - startTime) + " ms");
+        System.out.println("Number of primes: " + numPrimes);
+        System.out.println("Sum of primes: " + sumPrimes);
+
+        System.out.println("Iterations per thread: ");
+        for (String threadName : threadIterations.keySet()) {
+            System.out.println(threadName + ": " + threadIterations.get(threadName));
+        }
+
+        System.out.println("Time per thread: ");
+        for (String threadName : threadDurations.keySet()) {
+            System.out.println(threadName + ": " + threadDurations.get(threadName));
+        }
+    }
+
+    public void calculateNumAndSumPrimes(boolean primes[]) {
+        for (int i = 2; i < primes.length; i++) {
+            if(primes[i]) {
+                numPrimes++;
+                sumPrimes += i;
+            }
+        }
+    }
+
+    public void recordIterationsPerThread(String threadName, int iterations) {
+        threadIterations.put(threadName, threadIterations.getOrDefault(threadName, 0) + iterations);
+    }
+
+    public void recordDurationPerThread(String threadName, long duration) {
+        threadDurations.put(threadName, threadDurations.getOrDefault(threadName, 0L) + duration);
     }
 }
