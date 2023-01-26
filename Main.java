6fa35f58119112d4449711809a2713aa9e8691b4
Matthew Singh <mattsingh@hotmail.com>
2023-01-25 23:01:04 -0500

    Incremental sieve -- too slow

--- Main.java
+++ Main.java
@@ -8,64 +8,70 @@ public class Main {
     public static int MIN = 2;
     public static int MAX = (int) Math.pow(10, 8);
     public static int NUM_THREADS = 8;
-
+    // private boolean primes[] = new boolean[MAX];
+    
     public static void main(String[] args) throws InterruptedException {
+        List<int[]> primes = new SynchronizedArrayList<int[]>();
+        primes.add(new int[] { 3, 3 });
+
         ExecutorService threadExecutor = Executors.newFixedThreadPool(8);
-        List<PrimeFinderThread> threads = new ArrayList<>();
 
-        int range = (MAX - MIN) / NUM_THREADS;
-        for (int i = 0; i < NUM_THREADS; i++) {
-            int start = MIN + (i * range);
-            int end = start + range;
-            threads.add(new PrimeFinderThread(start, end));
+        for (int i = 2; i < MAX; i++) {
+            threadExecutor.execute(new PrimeFinderThread(i, primes));
         }
 
-        for (PrimeFinderThread thread : threads) {
-            threadExecutor.execute(thread);
-        }
         threadExecutor.shutdown();
         threadExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
     }
 }
 
 class PrimeFinderThread implements Runnable {
-    private int start;
-    private int end;
-    private List<Integer> primes;
+    private int n;
+    // private boolean primes[];
+    private List<int[]> primes;
+    private int primeCounter;
 
-    public PrimeFinderThread(int start, int end) {
-        this.start = start;
-        this.end = end;
-        this.primes = new ArrayList<>();
+    public PrimeFinderThread(int n, List<int[]> primes) {
+        this.n = n;
+        this.primes = primes;
+        this.primeCounter = 0;
     }
 
     @Override
     public void run() {
-        for (int i = start; i < end; i++) {
-            if (isPrime(i)) {
-                primes.add(i);
+        boolean isPrime = true;
+        for (int i = 0; i < primes.size(); i++) {
+            int prime[] = primes.get(i);
+            while(prime[1] < n)
+                prime[1] += prime[0];
+            if (prime[1] == n) {
+                isPrime = false;
+                break;
             }
         }
-        System.out.println(start + " - " + end);
-        System.out.println(primes.size());
+        if (isPrime) {
+            primes.add(new int[] { n, n });
+            primeCounter++;
+            System.out.println(n + " is prime");
+        }
+    }
+}
+
+class SynchronizedArrayList<T> extends ArrayList<T> {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public synchronized boolean add(T e) {
+        return super.add(e);
     }
 
-    private boolean isPrime(int number) {
-        // Corner cases
-        if (number <= 1)
-            return false;
-        if (number <= 3)
-            return true;
+    @Override
+    public synchronized T get(int index) {
+        return super.get(index);
+    }
 
-        // This is checked so that we can skip
-        // middle five numbers in below loop
-        if (number % 2 == 0 || number % 3 == 0)
-            return false;
-
-        for (int i = 5; i * i <= number; i = i + 6)
-            if (number % i == 0 || number % (i + 2) == 0)
-                return false;
-
-        return true;
+    @Override
+    public synchronized int size() {
+        return super.size();
     }
 }
